8adc9ba4ed7583848b376e3e83043fda9f31faa1
cesaromano <cerodema96@gmail.com>
2022-04-24 23:57:10 -0300

    comments added to encoder.py, main.py edited

--- encoder.py
+++ encoder.py
@@ -7,7 +7,9 @@ class Encoder:
         pass
 
     def binAdd(self, x, y):
+        """add one by one binary digits"""
 
+        #parse str value to int
         x = int(x)
         y = int(y)
 
@@ -17,15 +19,19 @@ class Encoder:
         return str(x+y)
 
     def grayToBin(self, gray):
+        """convert a binary sequence to gray code sequence"""
 
         if len(gray) == 1:
             return gray
         
+        #first bin of the new sequence is the same bin
         binl = []
         binl.append(gray[0])
+        #second bin of the new sequence is the addition betwen two first bins
         result = Encoder.binAdd(self, gray[0], gray[1])
         binl.append(result)
 
+        #addition betwen each bin result and next bin in the binary sequence
         for i in range(1, len(gray)-1):
 
             j = i+1
@@ -33,4 +39,29 @@ class Encoder:
             binl.append(result2)
             result = result2
 
-        return int(''.join(binl), 2)
+        #parse list to string
+        listToBinStr = ''.join(binl)
+
+        return listToBinStr
+
+    def binToGray(self, bin):
+        """convert a gray code sequence to binary sequence"""
+
+        if len(bin) == 1:
+            return bin
+
+        #first bin of the new sequence is the same bin
+        binl = []
+        binl.append(bin[0])
+
+        #addition betwen each gray bin sequence
+        for i in range(0, len(bin)-1):
+
+            j = i+1
+            result = Encoder.binAdd(self, bin[i], bin[j])
+            binl.append(result)
+
+        #parse list to string
+        listToGrayStr = ''.join(binl)
+
+        return listToGrayStr
--- main.py
+++ main.py
@@ -2,4 +2,23 @@ from encoder import Encoder
 
 test1 = Encoder()
 
-print(test1.grayToBin(str(1000)))
+#print(int(test1.grayToBin(str(1000)), 2))
+
+#print(test1.grayToBin(bin))
+
+#print(test1.binToGray(bin))
+
+bins = [format(x, "b") for x in range(0, 16)]
+grays = []
+decimal = []
+
+for each in range(0, len(bins)):
+    bintogray = test1.binToGray(bins[each])
+    print(bintogray)
+    grays.append(bintogray)
+
+for each in range(0, len(grays)):
+    graytobin = test1.grayToBin(grays[each])
+    print(graytobin)
+    bintodecimal = int(graytobin, 2)/100
+    print(bintodecimal)
